0c405306b6c7300f65081b7cfcba9dcb0ba456b7
Luis Leal <luisrrleal@gmail.com>
2025-11-06 18:41:05 -0600

    Add service

---
+++ services/scheduleService.ts
@@ -0,0 +1,19 @@
+'use server';
+
+export const fetchScheduleData = async (token: string) => {
+    const response = await fetch(`${process.env.API_URL}/movil/estudiante/horario`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        },
+
+    });
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch schedule data');
+    }
+
+    const data = await response.json();
+    return data;
+}   
